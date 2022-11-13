2095bb2c6025f2bb995850d1e55ad9b4d42b9ca3
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-11-13 10:40:35 -0800

    Recursive and Memoization solutions

---
+++ 70ClimbingStairs/solution.py
@@ -0,0 +1,39 @@
+# Recursive. Too slow.
+Recursive1
+
+def Recursive2climbStairs(self, n: int) -> int:
+    def recurse(current):
+        if current == n:
+            return 1
+        if current >= n:
+            return 0
+        return recurse(current+1) + recurse(current+2)
+    
+    return recurse(0)
+
+
+# Memoization. Too slow
+def Memo1climbStairs(self, n: int) -> int:
+    cache = {}
+    def dp(current):
+        if current in cache:
+            return cache[current]
+        if current == 1 or current == 0:
+            return 1
+        return dp(current-1) + dp(current-2)
+    
+    return dp(n)
+
+# Memoization second implementation.
+def Memo2climbStairs(self, n: int) -> int:
+    cache = {}
+    def dp(current):
+        if current in cache:
+            return cache[current]
+        if current == n:
+            return 1
+        if current >= n:
+            return 0
+        return dp(current+1) + dp(current+2)
+    
+    return dp(0)
