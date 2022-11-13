b4f178b008cd9cd5e44ca9284c4101942b008a46
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-11-13 14:52:49 -0800

    Finished DP

--- 70ClimbingStairs/solution.py
+++ 70ClimbingStairs/solution.py
@@ -1,3 +1,14 @@
+# True DP. Use last 2 results to calculate current. Fibonacci Sequence Alias
+def climbStairs(self, n: int) -> int:
+    if n <= 3:
+        return n
+    right, left = 1, 1
+    for x in range(n-1): # n-1 because it takes 1 step to go from n to n (if that makes sense). basically, left already takes care of it.
+        prev_left = left
+        left = right
+        right += prev_left
+    return right
+
 # Recursive. Too slow.
 def Recursive1climbStairs(self, n: int) -> int:
     def dp(current):
