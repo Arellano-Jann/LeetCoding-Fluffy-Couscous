667873e15654d25823cca1034ab3a95b82c520bf
Arellano Jann <89598304+Arellano-Jann@users.noreply.github.com>
2022-11-13 22:17:20 -0800

    added numbers

--- 198HouseRobber/solution.py
+++ 198HouseRobber/solution.py
@@ -7,8 +7,18 @@ def rob(self, nums: List[int]) -> int:
         right = max_rob
     return right
 
+# Iterative Memoization. Notice how dp[pos-1] and dp[pos] is the only one that is used. This is leveraged so that we know that we only need 2 variables to keep track of the last 2 calculated values.
+def rob2(self, nums: List[int]) -> int:
+    dp = [-1] * (len(nums) + 1)
+    dp[0] = 0
+    dp[1] = nums[0]
+    for pos in range(1, len(nums)):
+        dp[pos+1] = max(dp[pos-1] + nums[pos], dp[pos])
+            
+    return dp[-1]
+
 # Memoization. Stores DP
-def rob(self, nums: List[int]) -> int:
+def rob3(self, nums: List[int]) -> int:
     dp = [-1] * (len(nums) + 1)
     def rob2(pos):
         if dp[pos] >= 0:
@@ -20,7 +30,7 @@ def rob(self, nums: List[int]) -> int:
     return rob2(len(nums)-1)
 
 # Memoization using Cache decorator
-def rob(self, nums: List[int]) -> int:
+def rob4(self, nums: List[int]) -> int:
     @lru_cache(None)
     def rob2(pos):
         if pos < 0:
@@ -32,7 +42,7 @@ def rob(self, nums: List[int]) -> int:
 # Recursive Solution
 # Rob(pos) = max( Rob(pos-2) + nums[pos], Rob(pos-1) )
 # so maximum between the current AND the second to previous houses accumulated or the last house and everything accumulated
-def rob(self, nums: List[int]) -> int:
+def rob5(self, nums: List[int]) -> int:
     def rob2(pos):
         if pos < 0:
             return 0
